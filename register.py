8cc77e03cbfc39bdb25e1ab0f344b8d0a6eacae8
Darren Liew <darrenliew96@Inspiron-5437-Server.lan>
2023-09-24 22:06:35 +0800

    Deleted Redundant Files

--- register.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, Flask, request, render_template, session, redirect, jsonify
-from flask_session import Session
-from werkzeug.security import check_password_hash, generate_password_hash
-from sqlalchemy.orm.exc import NoResultFound, MultipleResultsFound
-from flask_sqlalchemy import SQLAlchemy
-
-register = Blueprint('register', __name__, template_folder='templates')
-
